266f2af9097846a55eb49be9b0b4c34ed7537eaa
Vaibhav <vbrana2014@gmail.com>
2023-12-03 01:26:00 +0530

    wornde

--- cplusequal.cpp
+++ cplusequal.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
-    
+    int T; cin >> T;
+    while (T--) {
+        int a, b, n; cin >> a >> b >> n;
+        int op = 0;
+        if (a > b) {
+            while (b < n) {
+                op += 1;
+                b += a;
+            }
+        } else {
+            while (a < n) {
+                op += 1;
+                a += b;
+            }
+        }
+        cout << op << endl;
+    } 
     return 0;
 }
